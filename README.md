08214e80a1227a3938ea03f20f8d6b7c11416736
Erik Dubois <erik.dubois@gmail.com>
2020-02-15 09:11:39 +0100

    copy paste of carli4

---
+++ README.md
@@ -0,0 +1,11 @@
+# ArcoLinuxIso Carli-5
+
+Start building your own carli version with 
+
+sudo ./build.sh
+
+To be able to build you install this package
+
+sudo pacman -S archiso
+
+To do : next - change name Arch Linux to Carli
---
+++ airootfs/root/customize_airootfs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e -u
+
+sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
+locale-gen
+
+ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+
+usermod -s /usr/bin/zsh root
+cp -aT /etc/skel/ /root/
+chmod 700 /root
+
+sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
+sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
+sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
+
+sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
+sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
+sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
+
+systemctl enable pacman-init.service choose-mirror.service
+systemctl set-default graphical.target
+systemctl enable sddm.service
+
+groupscarli="adm,audio,disk,floppy,log,network,optical,rfkill,storage,video,wheel,sys"
+useradd -m -g users -G $groupscarli -s /bin/bash liveuser
+passwd -d liveuser
